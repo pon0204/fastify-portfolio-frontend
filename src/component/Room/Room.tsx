70673c69a4a7c67c2a5b3c4aafc40af0a94b3467
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2021-09-22 09:32:59 +0900

    Layout.tsxにChat仕様を追加

--- src/component/Room/Room.tsx
+++ src/component/Room/Room.tsx
@@ -1,8 +1,8 @@
 import React,{ useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { useAppSelector } from '../../app/hooks'
 import { selectChat } from '../../slices/chatSlice'
-import { selectEditedRoom, selectEditedRoomMode, setRoomEditMode } from '../../slices/roomSlice'
-import { Chat, Chats, editedRoom } from '../../types/types'
+import { selectEditedRoomMode } from '../../slices/roomSlice'
+import { Chat } from '../../types/types'
 import ChatBox from './ChatAsset/ChatBox'
 import InputForm from './ChatAsset/InputForm'
 import RoomConfigModal from './RoomConfigModal'
@@ -11,7 +11,6 @@ import UserSelectTabs from './ChatAsset/UserSelectTabs'
 const Room = () => {
   const chatData:Chat[] = useAppSelector(selectChat)
   const RoomEditMode: boolean = useAppSelector(selectEditedRoomMode)
-  const dispatch = useAppDispatch()
 
   const ScrollBottom = (target:HTMLElement) => {
     if (target) {
@@ -27,14 +26,10 @@ const Room = () => {
     }, [chatData])
 
   return (
-    <div className='pt-24 px-20'>
+    <div className='px-20 pt-40'>
       {RoomEditMode &&
         <RoomConfigModal/>
       }
-      <div className='fixed top-1 right-4 flex gap-2'>
-        <button onClick={() => dispatch(setRoomEditMode(true))} className='w-40 bg-gray-400 p-4 text-white text-center font-bold text-md'>編 集</button>
-        <button className='w-40 bg-red-500 p-4 text-white text-center font-bold text-md'>保 存</button>
-      </div>
       {
       chatData.map((chat:Chat,index:number) => (
         <ChatBox isMe={chat.isMe} text={chat.text} key={index}/>
